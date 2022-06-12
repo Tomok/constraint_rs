5dfd309c3e558f2c3519cbf14cf918422998376d
Thomas Lahmer <791550+Tomok@users.noreply.github.com>
2022-06-12 14:46:13 +0200

    created black-box test for empty struct

--- constraint_rs_test/src/lib.rs
+++ constraint_rs_test/src/lib.rs
@@ -1,5 +1,20 @@
 #[cfg(test)]
 mod tests {
+
+    mod empty_struct {
+        use constraint_rs_derive::ConstraintType;
+
+        #[derive(ConstraintType)]
+        struct TestStruct {}
+
+        #[test]
+        fn test() {
+            let cfg = z3::Config::new();
+            let ctx = constraint_rs::Context::new(&cfg);
+            let ct = TestStruct::constraint_type(&ctx);
+            // currently no functions on ct ... but being able to create it is also a good test
+        }
+    }
     #[test]
     fn it_works() {
         let result = 2 + 2;
