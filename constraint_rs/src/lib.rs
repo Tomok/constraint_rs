a3a1fe41c87fff182add28bbb93a1ebaebb26cf6
Thomas Lahmer <791550+Tomok@users.noreply.github.com>
2022-06-16 18:06:04 +0000

    Added ConstrainedValue

--- constraint_rs/src/lib.rs
+++ constraint_rs/src/lib.rs
@@ -15,8 +15,26 @@ pub trait ConstrainedType<'s, 'ctx>
 where
     'ctx: 's,
 {
+    type ValueType: ConstrainedValue<'s, 'ctx>;
+
     fn new(context: &'s Context<'ctx>) -> Self;
+    fn fresh_value(&'s self, name_prefix: &str) -> Self::ValueType;
 }
+
+/// A constant variable to be defined via constraints
+pub trait ConstrainedValue<'s, 'ctx>
+where
+    'ctx: 's,
+{
+    type ValueType: HasConstrainedType<'s, 'ctx>;
+
+    ///get a possible set of values from the given Model
+    fn eval(&'s self, model: &Model<'ctx>) -> Option<Self::ValueType>;
+
+    //todo: add functions to iter all solutions
+}
+
+pub type Model<'ctx> = z3::Model<'ctx>;
 
 #[derive(Clone)]
 pub struct DataType<'ctx>(Rc<z3::DatatypeSort<'ctx>>);
@@ -24,6 +42,12 @@ pub struct DataType<'ctx>(Rc<z3::DatatypeSort<'ctx>>);
 impl<'ctx> DataType<'ctx> {
     fn new(dt: z3::DatatypeSort<'ctx>) -> Self {
         Self(Rc::new(dt))
+    }
+
+    /// get the [z3::DatatypeSort] this should not be necessary to be called outside of generated
+    /// code
+    pub fn z3_datatype_sort(&self) -> &Rc<z3::DatatypeSort<'ctx>> {
+        &self.0
     }
 }
 
@@ -54,6 +78,12 @@ impl<'ctx> Context<'ctx> {
             .entry(name)
             .or_insert_with(|| DataType::new(datatype_build_fn(self.z3_context)))
             .clone()
+    }
+
+    /// get the [z3::Context] ... this should only be necessary if you do not use the dervice
+    /// mechanisms and want to implement a [DataType]
+    pub fn z3_context(&self) -> &'ctx z3::Context {
+        self.z3_context
     }
 }
 
@@ -91,9 +121,12 @@ mod tests {
     mod empty_struct_derived_code_test {
         use std::ptr;
 
+        use z3::ast::Ast;
+
         use super::super::*;
         /// Struct for which the derived types and functions are listed below & used for this test
-        struct Empty();
+        #[derive(Debug, PartialEq, Eq)]
+        pub struct Empty();
 
         pub struct EmptyConstrainedType<'s, 'ctx> {
             context: &'s Context<'ctx>,
@@ -104,6 +137,8 @@ mod tests {
         where
             'ctx: 's,
         {
+            type ValueType = EmptyConstrainedValue<'s, 'ctx>;
+
             fn new(context: &'s Context<'ctx>) -> Self {
                 let data_type = context.enter_or_get_datatype("TestStruct", |c| {
                     z3::DatatypeBuilder::new(c, "TestStruct")
@@ -111,6 +146,15 @@ mod tests {
                         .finish()
                 });
                 Self { context, data_type }
+            }
+
+            fn fresh_value(&'s self, name_prefix: &str) -> Self::ValueType {
+                let val = z3::ast::Datatype::fresh_const(
+                    self.context.z3_context(),
+                    name_prefix,
+                    &self.data_type.z3_datatype_sort().sort,
+                );
+                Self::ValueType { val, typ: self }
             }
         }
 
@@ -139,6 +183,42 @@ mod tests {
             let ts1 = Empty::constrained_type(&context);
             let ts2 = Empty::constrained_type(&context);
             assert!(ptr::eq(ts1.data_type.0.as_ref(), ts2.data_type.0.as_ref()));
+        }
+
+        pub struct EmptyConstrainedValue<'s, 'ctx> {
+            typ: &'s EmptyConstrainedType<'s, 'ctx>,
+            val: z3::ast::Datatype<'ctx>,
+        }
+
+        impl<'s, 'ctx> ConstrainedValue<'s, 'ctx> for EmptyConstrainedValue<'s, 'ctx>
+        where
+            'ctx: 's,
+        {
+            type ValueType = Empty;
+
+            fn eval(&'s self, model: &Model<'ctx>) -> Option<Self::ValueType> {
+                // as the type is empty, it is always the following, as long as the model exists
+                Some(Empty())
+            }
+        }
+
+        #[test]
+        fn constrained_value() {
+            let config = z3::Config::new();
+            let ctx = z3::Context::new(&config);
+            let context = Context::new(&ctx);
+            let typ = Empty::constrained_type(&context);
+            let val1 = typ.fresh_value("val1");
+            let solver = z3::Solver::new(&ctx); //todo - do not call z3 directly once corresponding methods was implemented
+            assert_eq!(z3::SatResult::Sat, solver.check());
+            let model = solver.get_model().unwrap();
+            assert_eq!(Some(Empty()), val1.eval(&model));
+
+            //assert something impossible (Empty type != empty type) to se solver failing
+            let val2 = typ.fresh_value("val2");
+            solver.assert(&val2.val._safe_eq(&val1.val).expect("Type missmatch").not());
+            assert_eq!(z3::SatResult::Unsat, solver.check());
+            assert!(solver.get_model().is_none());
         }
     }
 }
--- constraint_rs_test/src/lib.rs
+++ constraint_rs_test/src/lib.rs
@@ -4,20 +4,24 @@ mod tests {
     mod empty_struct {
         use constraint_rs_derive::ConstraintType;
 
-        use constraint_rs::HasConstrainedType;
+        use constraint_rs::{ConstrainedType, ConstrainedValue, HasConstrainedType};
 
-        #[derive(ConstraintType)]
-        struct TestStruct {}
+        #[derive(ConstraintType, PartialEq, Eq, Debug)]
+        pub struct TestStruct();
 
         #[test]
         fn test() {
             let cfg = z3::Config::new();
             let z3_context = z3::Context::new(&cfg);
             let context = constraint_rs::Context::new(&z3_context);
-            {
-                let ct = TestStruct::constrained_type(&context);
-                drop(ct);
-            } // currently no functions on ct ... but being able to create it is also a good test
+            let constrained_type = TestStruct::constrained_type(&context);
+            let constrained_value = constrained_type.fresh_value("v");
+            //todo: should not be necessary to call z3 directly in the future...
+            let solver = z3::Solver::new(&z3_context);
+            assert_eq!(z3::SatResult::Sat, solver.check());
+            let model = solver.get_model().unwrap();
+            let value = constrained_value.eval(&model).unwrap();
+            assert_eq!(TestStruct(), value);
         }
     }
     #[test]
