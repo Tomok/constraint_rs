8d344f64a8c8244266dae6051f1fec938d0a9a10
Thomas Lahmer <791550+Tomok@users.noreply.github.com>
2022-06-20 19:16:25 +0000

    implemented derive new for unamed structs still missing eval implementation

--- constraint_rs_derive/src/lib.rs
+++ constraint_rs_derive/src/lib.rs
@@ -109,28 +109,60 @@ impl DerivedFields {
     //todo: find better name ... new seams wrong here, as syn::* input is converted
     fn new(data_struct: syn::DataStruct, ident: &syn::Ident) -> Self {
         let str_ident = format!("{}", ident);
-        let constrained_type_new_fn = match data_struct.fields {
+        let constrained_type_new_fn = match &data_struct.fields {
             syn::Fields::Named(_) => todo!(),
-            syn::Fields::Unnamed(_) => todo!(),
+            syn::Fields::Unnamed(fields) => {
+                let field_entries = fields
+                    .unnamed
+                    .iter()
+                    .enumerate()
+                    .map(|(i, f)| (format!("{}", i), Self::field_for_datatype_type_field(f)))
+                    .map(|(i, f)| {
+                        let t: syn::Expr = syn::parse_quote!(
+                            (#i, z3::DatatypeAccessor::Sort(#f)),
+                        );
+                        t
+                    });
+                let fields: syn::Expr = syn::parse_quote!(vec![#(#field_entries),*]);
+                syn::parse_quote!(
+                    fn new(context: &'s constraint_rs::Context<'ctx>) -> Self {
+                        let data_type = context.enter_or_get_datatype(#str_ident, |c| {
+                        z3::DatatypeBuilder::new(c, #str_ident)
+                            .variant("", #fields)
+                            .finish()
+                        });
+                        Self {
+                            context,
+                            data_type
+                        }
+                    }
+
+                )
+            }
             syn::Fields::Unit => {
                 syn::parse_quote!(
                     fn new(context: &'s constraint_rs::Context<'ctx>) -> Self {
-                    let data_type = context.enter_or_get_datatype(#str_ident, |c| {
+                        let data_type = context.enter_or_get_datatype(#str_ident, |c| {
                         z3::DatatypeBuilder::new(c, #str_ident)
                             .variant("", vec![])
                             .finish()
-                    });
-                    Self {
-                        context,
-                        data_type
+                        });
+                        Self {
+                            context,
+                            data_type
+                        }
                     }
-                })
+                )
             }
         };
 
-        let constrained_value_eval_fn = match data_struct.fields {
+        let constrained_value_eval_fn = match &data_struct.fields {
             syn::Fields::Named(_) => todo!(),
-            syn::Fields::Unnamed(_) => todo!(),
+            syn::Fields::Unnamed(_) => syn::parse_quote!( //TODO eval fields
+                fn eval(&'s self, model: &constraint_rs::Model<'ctx>,) -> Option<Self::ValueType>{
+                    Some(#ident())
+                }
+            ),
             syn::Fields::Unit => syn::parse_quote!(
                 fn eval(&'s self, model: &constraint_rs::Model<'ctx>,) -> Option<Self::ValueType>{
                     Some(#ident)
@@ -140,6 +172,31 @@ impl DerivedFields {
         Self {
             constrained_type_new_fn,
             constrained_value_eval_fn,
+        }
+    }
+
+    fn field_for_datatype_type_field(field: &syn::Field) -> syn::Expr {
+        match &field.ty {
+            syn::Type::Array(_) => todo!(),
+            syn::Type::BareFn(_) => todo!(),
+            syn::Type::Group(_) => todo!(),
+            syn::Type::ImplTrait(_) => todo!(),
+            syn::Type::Infer(_) => todo!(),
+            syn::Type::Macro(_) => todo!(),
+            syn::Type::Never(_) => todo!(),
+            syn::Type::Paren(_) => todo!(),
+            syn::Type::Path(p) => {
+                syn::parse_quote!(
+                    z3::DatatypeAccessor::Sort(#p::constrained_type(context).z3_sort().clone()),
+                )
+            }
+            syn::Type::Ptr(_) => todo!(),
+            syn::Type::Reference(_) => todo!(),
+            syn::Type::Slice(_) => todo!(),
+            syn::Type::TraitObject(_) => todo!(),
+            syn::Type::Tuple(_) => todo!(),
+            syn::Type::Verbatim(_) => todo!(),
+            _ => todo!(),
         }
     }
 }
