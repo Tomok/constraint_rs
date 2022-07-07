1dc0010d1eda0f8738caf03c8248039fa3941f5a
Thomas Lahmer <791550+Tomok@users.noreply.github.com>
2022-07-07 18:05:31 +0000

    added test to check creating the same datatype twice and setting objects of that type equal

--- constraint_rs_test/src/lib.rs
+++ constraint_rs_test/src/lib.rs
@@ -111,5 +111,37 @@ mod tests {
                 value
             );
         }
+
+        #[test]
+        fn create_datatype_twice() {
+            let cfg = z3::Config::new();
+            let z3_context = z3::Context::new(&cfg);
+            let context = constraint_rs::Context::new(&z3_context);
+            let constrained_type1 = TestStruct::constrained_type(&context);
+            let constrained_type2 = TestStruct::constrained_type(&context);
+            let constrained_value1 = constrained_type1.fresh_value("v1");
+            let constrained_value2 = constrained_type2.fresh_value("v2");
+            let solver = z3::Solver::new(&z3_context);
+
+            constrained_value1.assign_value(
+                &solver,
+                &TestStruct {
+                    field1: 1,
+                    field2: 2,
+                },
+            );
+            solver.assert(constrained_value2._eq(&constrained_value1).val());
+            //todo: should not be necessary to call z3 directly in the future...
+            assert_eq!(z3::SatResult::Sat, solver.check());
+            let model = solver.get_model().unwrap();
+            let value = constrained_value2.eval(&model).unwrap();
+            assert_eq!(
+                TestStruct {
+                    field1: 1,
+                    field2: 2
+                },
+                value
+            );
+        }
     }
 }
