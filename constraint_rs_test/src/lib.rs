ca079f2e06c76619bc15cc874e736b8aac1896ab
Thomas Lahmer <791550+Tomok@users.noreply.github.com>
2022-07-07 15:04:41 +0000

    added test for _eq

--- constraint_rs_test/src/lib.rs
+++ constraint_rs_test/src/lib.rs
@@ -50,15 +50,32 @@ mod tests {
                 &val._safe_eq(&z3::ast::BV::from_u64(&z3_context, 7, 32))
                     .unwrap(),
             );
-            /*let f_accessor = typ.data_type.0.variants[0].accessors[0].apply(&[&val1.val]);
-            solver.assert(
-                &f_accessor
-                    .as_bv()
-                    .unwrap()
-                    ._safe_eq(&z3::ast::BV::from_u64(&ctx, 42, 64))
-                    .unwrap(),
-            );*/
 
+            assert_eq!(z3::SatResult::Sat, solver.check());
+            let model = solver.get_model().unwrap();
+            let value = constrained_value.eval(&model).unwrap();
+            assert_eq!(TestStruct { field1: 7 }, value);
+        }
+
+        #[test]
+        fn test_eq() {
+            let cfg = z3::Config::new();
+            let z3_context = z3::Context::new(&cfg);
+            let context = constraint_rs::Context::new(&z3_context);
+            let constrained_type = TestStruct::constrained_type(&context);
+            let constrained_value = constrained_type.fresh_value("v");
+            //todo: should not be necessary to call z3 directly in the future...
+            let solver = z3::Solver::new(&z3_context);
+            let constrained_u32_type = u32::constrained_type(&context);
+            let value7 = constrained_u32_type.fresh_value("value7");
+            solver.assert(
+                &value7
+                    .val()
+                    ._safe_eq(&z3::ast::BV::from_u64(&z3_context, 7, 32))
+                    .unwrap(),
+            );
+
+            solver.assert(constrained_value.field1._eq(&value7).val());
             assert_eq!(z3::SatResult::Sat, solver.check());
             let model = solver.get_model().unwrap();
             let value = constrained_value.eval(&model).unwrap();
